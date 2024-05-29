e751ce7e825410683b785dd8e3ee9be13431b1c1
John Doe <johndoe@example.com>
2024-05-29 23:46:34 +0200

    First commit, added a database

---
+++ app.py
@@ -0,0 +1,34 @@
+import os
+from flask import Flask, render_template, request, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+from sqlalchemy.sql import func
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+        'sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# ...
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    type = db.Column(db.String(100), nullable=False)
+    bio = db.Column(db.Text)
+    
+
+    def __repr__(self):
+        return f'<Produkt {self.title}>'
+    
+# ...
+
+@app.route('/sklepik')
+def index():
+    products = Product.query.all()
+    return render_template('sklepik.html', products=products)    
