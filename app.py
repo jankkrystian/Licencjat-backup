abb34e3a361d0d7de4a989bc966ba0505a79200f
Twoje Imię <twojemail@example.com>
2024-06-18 13:12:39 +0200

    pierwsza wersja kodu, dziala

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
 import os
 from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 from sqlalchemy.sql import func
 
-
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 app.config['SQLALCHEMY_DATABASE_URI'] =\
         'sqlite:///' + os.path.join(basedir, 'database.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-
+# migrate = Migrate(app, db)
 # ...
+
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -27,7 +28,6 @@ class Product(db.Model):
     bio_fact = db.Column(db.Text)
     bio_info = db.Column(db.Text)
     image = db.Column(db.String(200), nullable=False)
-
 
     def __repr__(self):
         return f'<Produkt {self.title}>'
@@ -43,10 +43,15 @@ def sklepik():
 def home():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
-
-@app.route('/<int:product_id>/')
+@app.route('/produkt/<product_id>')
 def product(product_id):
     product = Product.query.get_or_404(product_id)
     return render_template('produkt.html', product=product) 
+
+@app.route('/adnotacja')
+def adnotacja():
+    return render_template('adnotacja.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
